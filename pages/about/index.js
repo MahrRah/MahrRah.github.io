dddb9229cc4d3cbe83ceed1a033ce494fcbf005f
Tamara Rahimi <mahra.rah@gmail.com>
2024-07-25 08:33:58 +0200

    overlap

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,9 +5,9 @@ import Blackhole from '../components/BlackholeStatic';
 
 export default function About() {
     return (
-        <Box sx={{ alignItems: 'center',justifyContent:'center' }}>
-        <Blackhole status="static"/>
-        <Typography className="title">
+      <Box sx={{ position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
+        <Blackhole status="static" />
+        <Typography className="title" sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
           asdasdasd
         </Typography>
       </Box>
