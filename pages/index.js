a30bef922b077bea7332e69eb4cc3d10ca7ac66f
Tamara Rahimi <mahra.rah@gmail.com>
2024-08-01 13:04:44 +0200

    Add about me section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { useTransform, useScroll, useTime } from 'framer-motion';
 import { degreesToRadians } from 'popmotion';
+import { styled } from '@mui/system';
 import Scene from '../components/BlackholeScene';
 import Banner from '../components/Banner';
 import NavigationBar from '../components/Navigation';
@@ -17,6 +20,7 @@ const styles = {
   },
   container: {
     height: '400vh', // Makes the div content scrollable
+    alignContent: 'baseline',
   },
   canvasContainer: {
     height: '30vh',
@@ -26,7 +30,51 @@ const styles = {
     left: 0,
     zIndex: -1, // Ensure the canvas is behind other content
   },
+  aboutMe: {
+    position: 'fixed',
+    bottom: '50%', // Add a small separation from the bottom
+    left: 0,
+    transform: 'translate(15px, 50%)',
+    width: '50vw',
+    backgroundColor: 'black',
+    color: 'white',
+    padding: '30px',
+    borderRadius: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  textContainer: {
+    textAlign: 'left',
+    hight: '100vh',
+  },
+  heading: {
+    margin: '0 0 10px 0',
+    fontSize: '24px',
+    fontFamily: 'lemon-milk',
+
+  },
+  button_wrapper: { textAlign: 'center' },
+  button: {
+    marginTop: '20px',
+    fontSize: '15px',
+    fontFamily: 'lemon-milk',
+    color: '#CEBE9C',
+    borderColor: '#CEBE9C',
+    borderRadius: 28
+  },
+  paragraph: {
+    margin: '0',
+    fontSize: '16px',
+    textAlign: 'justify',
+  },
 };
+const StyledAvatar = styled(Avatar)({
+  width: 150,
+  height: 150,
+  marginBottom: 20,
+});
 
 const HomePage = () => {
   const [isBannerHidden, setIsBannerHidden] = useState(false);
@@ -34,6 +82,11 @@ const HomePage = () => {
   const [currentPage, setPage] = useState(Page.Home);
   const [shouldScroll, setShouldScroll] = useState(true);
   const [lookAtCoordinates, setLookAtCoordinates] = useState([0, 0, 0]);
+
+
+
+  const aboutMeText = "Hi there, I am Mahra, a Software Engineer at Microsoft with a background in astrophysics. Basically, a physics nerd who has lost her way in the world of tech. I used to analyze emissions from galaxies containing active supermassive black holes. Now I work on various cloud-based solutions as part of one of many Microsoft ISE teams. These range from cloud to edge-based applications for manufacturing use cases to LLM-based code generation solutions.";
+
 
   const handleScroll = () => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
@@ -51,13 +104,13 @@ const HomePage = () => {
   const switchScene = (page) => {
     setPage(page);
     setShouldScroll(false);
-    setLookAtCoordinates([0, -2, 0]);
+    setLookAtCoordinates([0, -1, 0]);
     if (page === Page.Home) {
       setShouldScroll(true);
-      window.scrollTo(0, 0); 
+      window.scrollTo(0, 0);
     }
     else {
-      window.scrollTo(0, document.body.scrollHeight); 
+      window.scrollTo(0, document.body.scrollHeight);
     }
   };
 
@@ -76,6 +129,16 @@ const HomePage = () => {
         </div>
       </div>
       <NavigationBar isVisible={isNavVisible} currentPage={currentPage} setPage={switchScene} />
+      { currentPage==Page.About && isNavVisible && <div style={styles.aboutMe}>
+        <StyledAvatar alt="User Avatar" src="/avatar.jpg" />
+        <div style={styles.textContainer}>
+          <h1 style={styles.heading}>About Me</h1>
+          <p style={styles.paragraph}>{aboutMeText}</p>
+          <div style={styles.button_wrapper}>
+            <Button variant="outlined" sx={styles.button} href="mailto:mahra.rah@gmail.com">Contact Me</Button>
+          </div>
+        </div>
+      </div>}
     </div>
   );
 };
@@ -95,7 +158,7 @@ function MyCameraReactsToStateChanges({ shouldScroll }) {
     if (shouldScroll) {
       camera.lookAt(0, 0, 0);
     } else {
-      camera.lookAt(2, -2, 0);
+      camera.lookAt(0, -2, 0);
     }
 
   });
