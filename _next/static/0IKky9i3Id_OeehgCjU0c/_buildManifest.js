d13f9e4dc180b5e4185b79c8740308894f850a90
MahrRah <mahra.rah@gmail.com>
2024-08-03 21:03:19 +0000

    Deploying to gh-pages from @ MahrRah/MahrRah.github.io@db93f6c37e10ed8d25ee4f85259f623bd42aa4d4 🚀

---
+++ _next/static/0IKky9i3Id_OeehgCjU0c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-f7bb2c45e2810869.js","static/chunks/946-8d9442ea49c9cdb0.js","static/chunks/pages/index-268d3827bff36d3e.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/zskGi39sZXEXHg_OoLU3_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-f7bb2c45e2810869.js","static/chunks/946-8d9442ea49c9cdb0.js","static/chunks/pages/index-1a64a826190593e2.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
