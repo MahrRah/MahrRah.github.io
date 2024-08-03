a04a44ac43e17b65e693f0a3d30505e7bc12fde7
MahrRah <mahra.rah@gmail.com>
2024-08-03 12:09:32 +0000

    Deploying to gh-pages from @ MahrRah/MahrRah.github.io@50fcd08e3506b6a686b40b56004ee5a6371afe2b 🚀

---
+++ _next/static/YC_zaeKQTjEHMs9px3iBZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-5a532a818777a216.js","static/chunks/87-617a6e3e45daa3fd.js","static/chunks/pages/index-93e16314979db17b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
